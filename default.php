1d8e1fc8d53a91e1940d7cd2c590f68f369072de
kasperisager <kasperisager@gmail.com>
2014-01-24 15:56:24 -0500

    Lint source code

--- default.php
+++ default.php
@@ -1,71 +1,70 @@
 <?php if (!defined('APPLICATION')) exit();
 
-$PluginInfo['WordCount'] = array(
-   'Name'         => 'Word Count',
-   'Description'  => 'Word Count provides you with accurate word and character counts when composing discussions and comments.',
-   'Version'      => '1.1',
-   'Author'       => 'Kasper K. Isager',
-   'AuthorEmail'  => 'kasperisager@gmail.com',
-   'AuthorUrl'    => 'http://github.com/kasperisager'
+$PluginInfo['wordcount'] = array(
+    'Name'        => 'Word Count',
+    'Description' => 'Word Count provides you with accurate word and character counts when composing discussions and comments.',
+    'Version'     => '1.1',
+    'Author'      => 'Kasper K. Isager',
+    'AuthorEmail' => 'kasperisager@gmail.com',
+    'AuthorUrl'   => 'http://github.com/kasperisager'
 );
 
 /**
- * WordCount plugin for Vanilla
+ * "Word Count" plugin for Vanilla
  *
- * @package    Addons
- * @version    1.1
- * @author     Kasper Kronborg Isager <kasperisager@gmail.com>
- * @copyright  Copyright © 2013
- * @license    http://opensource.org/licenses/MIT MIT
+ * @version   1.1
+ * @author    Kasper Kronborg Isager <kasperisager@gmail.com>
+ * @copyright Copyright © 2013
+ * @license   http://opensource.org/licenses/MIT MIT
  */
 class WordCount extends Gdn_Plugin
 {
-   /**
-    * Add WordCount to post pages
-    * 
-    * @param  Gdn_Controller $Sender
-    * @since  1.0
-    * @access public
-    */
-   public function PostController_Render_Before($Sender)
-   {
-      $this->_WordCounter($Sender);
-   }
+    /**
+     * Add Word Count to post pages
+     *
+     * @since  1.0
+     * @param  PostController $Sender
+     * @access public
+     */
+    public function PostController_Render_Before($Sender)
+    {
+        $this->_WordCounter($Sender);
+    }
 
-   /**
-    * Add WordCount to discussion pages
-    * 
-    * @param  Gdn_Controller $Sender
-    * @since  1.0
-    * @access public
-    */
-   public function DiscussionController_Render_Before($Sender)
-   {
-      $this->_WordCounter($Sender);
-   }
+    /**
+     * Add Word Count to discussion pages
+     *
+     * @since  1.0
+     * @param  DiscussionController $Sender
+     * @access public
+     */
+    public function DiscussionController_Render_Before($Sender)
+    {
+        $this->_WordCounter($Sender);
+    }
 
-   /**
-    * WordCount resources
-    * 
-    * @param  Gdn_Controller $Sender
-    * @since  1.0
-    * @access private
-    */
-   private function _WordCounter($Sender)
-   {
-      // Stylesheet
-      $Sender->AddCssFile('jquery.wordcount.css', 'plugins/WordCount');
+    /**
+     * Word Count resources
+     *
+     * @since  1.0
+     * @param  Gdn_Controller $Sender
+     * @access private
+     */
+    private function _WordCounter($Sender)
+    {
+        // Stylesheet
+        $Sender->AddCssFile('jquery.wordcount.css', 'plugins/WordCount');
 
-      // Javascript
-      $Sender->AddJsFile('jquery.wordcount.js', 'plugins/WordCount');
+        // Javascript
+        $Sender->AddJsFile('jquery.wordcount.js', 'plugins/WordCount');
 
-      // Config definitions
-      $Sender->AddDefinition('MaxCount', C('Vanilla.Comment.MaxLength'));
+        // Config definitions
+        $Sender->AddDefinition('MaxCount', C('Vanilla.Comment.MaxLength'));
 
-      // Translateable labels
-      $Sender->AddDefinition('CharLabel', T('character left'));
-      $Sender->AddDefinition('CharsLabel', T('characters left'));
-      $Sender->AddDefinition('WordLabel', T('word'));
-      $Sender->AddDefinition('WordsLabel', T('words'));
-   }
+        // Translateable labels
+        $Sender->AddDefinition('CharLabel', T('character left'));
+        $Sender->AddDefinition('CharsLabel', T('characters left'));
+        $Sender->AddDefinition('WordLabel', T('word'));
+        $Sender->AddDefinition('WordsLabel', T('words'));
+    }
 }
--- js/jquery.wordcount.js
+++ js/jquery.wordcount.js
@@ -1,27 +1,8 @@
-/**
- *  Copyright 2013 Kasper Kronborg Isager
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 jQuery(function() {
-
     var forms = $(".DiscussionForm, .CommentForm, .EditCommentForm");
 
     $(forms).livequery(function() {
-
         $(this).find(".TextBox").each(function() {
-
             var $this = $(this);
 
             var maxCount = gdn.definition("MaxCount"),
@@ -32,12 +13,12 @@ jQuery(function() {
                 wordsLabel = gdn.definition("WordsLabel");
 
             countMsg.find("ul")
-                .append("<li class='Characters'>" 
+                .append("<li class='Characters'>"
                     + "<span class='Count'></span> "
                     + "<span class='Label'></span>"
                     + "</li>"
                 )
-                .append("<li class='Words'>" 
+                .append("<li class='Words'>"
                     + "<span class='Count'></span> "
                     + "<span class='Label'></span>"
                     + "</li>"
@@ -49,25 +30,20 @@ jQuery(function() {
             wordCount();
 
             var initialVal = $this.val();
-      
-            $this.change(function(e){
 
+            $this.change(function (e) {
                 charCount();
                 wordCount();
-
             });
-      
+
             setInterval(function () {
-        
                 if ($this.val() != initialVal) {
                     $this.change();
                     initialVal = $this.val();
                 }
-        
             }, 100);
 
             function charCount() {
-
                 var count = (maxCount - $this.val().length);
 
                 countMsg.find(".Characters .Count").html(count);
@@ -85,11 +61,9 @@ jQuery(function() {
                 } else {
                     countMsg.find(".Characters .Label").html(charLabel);
                 }
-
             }
 
             function wordCount() {
-
                 var count = $this.val();
 
                 if (count === "") {
@@ -105,11 +79,7 @@ jQuery(function() {
                 } else {
                     countMsg.find(".Words .Label").html(wordLabel);
                 }
-
             }
-
         });
-  
     });
-
 });
